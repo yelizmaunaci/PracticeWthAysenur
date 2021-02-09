3bd37e05d1ede32a8a817d4e14076132e29fda87
yelizmaunaci <yeliz_maunaci@hotmail.com>
2021-02-09 09:24:36 -0500

    day 1

--- src/test/java/day1_intro/TitleVerification.java
+++ src/test/java/day1_intro/TitleVerification.java
@@ -1,7 +1,27 @@
 package day1_intro;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class TitleVerification {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+
+ // setup chrome driver
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver= new ChromeDriver();
+
+        driver.manage().window().maximize();
+
+        driver.get("https://www.google.com");
+        System.out.println("current Title= "+ driver.getTitle());
+
+        String currentUrl= driver.getCurrentUrl();
+        System.out.println("current Url= "+currentUrl);
+
+        Thread.sleep(2500);
+
+        driver.close();
 
     }
 }
